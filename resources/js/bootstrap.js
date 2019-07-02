47ec348f9b4f5f34bd7e3dc2399adb05cbe1dba0
zuhri <zuhri@sleekr.co>
2019-07-02 21:42:37 +0700

    now bootstrap functional with js also

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -24,6 +24,13 @@ if (token) {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
+try {
+    window.$ = window.jQuery = require('jquery');
+    window.Popper = require('popper.js').default;
+
+    require('bootstrap');
+} catch (e) {}
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting

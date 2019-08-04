7f6a520005d58c5829b2da21221e3c0c52822ff2
zuhri <zuhri@sleekr.co>
2019-08-04 23:54:52 +0700

    VehicleController with full API

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -28,7 +28,22 @@ class VehicleController extends Controller
             'query' => $results->getValidated(),
             'vehicles' => $results->getCollection(),
         ];
-        return view('vehicle.index', $data);
+        return $this->renderView($request, 'vehicle.index', $data, [], 200);
+    }
+
+    public function show(
+        Request $request,
+        Vehicle $vehicle,
+        VehiclePresenter $presenter
+    ) {
+        if (!$this->allowUser('superadmin-only')) {
+            return back()->with('error', __("authorize.not_superadmin"));
+        }
+
+        $data = [
+            'vehicle' => $presenter->transform($vehicle),
+        ];
+        return $this->renderView($request, '', $data, [], 200);
     }
 
     public function create()
@@ -50,7 +65,7 @@ class VehicleController extends Controller
 
         $validated = $request->validated();
         $service->perform($validated);
-        return redirect()->route('vehicles.index');
+        return $this->renderView($request, '', [], ['route' => 'vehicles.index', 'data' => []], 201);
     }
 
     public function edit(Vehicle $vehicle)
@@ -73,16 +88,16 @@ class VehicleController extends Controller
 
         $validated = $request->validated();
         $service->perform($validated, $vehicle);
-        return redirect()->route('vehicles.edit', ['vehicle' => $vehicle->id]);
+        return $this->renderView($request, '', [], ['route' => 'vehicles.edit', 'data' => ['vehicle' => $vehicle->id]], 204);
     }
 
-    public function destroy(Vehicle $vehicle)
+    public function destroy(Request $request, Vehicle $vehicle)
     {
         if (!$this->allowUser('superadmin-only')) {
             return back()->with('error', __("authorize.not_superadmin"));
         }
 
         $vehicle->delete();
-        return redirect()->route('vehicles.index');
+        return $this->renderView($request, '', [], ['route' => 'vehicles.index', 'data' => []], 204);
     }
 }

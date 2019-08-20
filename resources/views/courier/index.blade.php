c48035aee7cbedc40a9b153a97e9a932fa4d54f9
erwin kurniawan <erwin@sleekr.co>
2019-08-20 09:15:49 +0700

    integration fe

--- resources/views/courier/index.blade.php
+++ resources/views/courier/index.blade.php
@@ -1,45 +1,17 @@
 @extends('layouts.app')
+@section('title', 'List Courier')
 
 @section('content')
-<div class="card">
-    <div class="card-header">Kurir</div>
-
-    <div class="card-body">
-        <a href="{{ route('couriers.create') }}" class="btn btn-primary">Buat</a>
-
-        @if ($couriers->count() > 0 || !empty($query))
-            @include('includes/index_navigation')
-            <table class="table">
-                <thead>
-                    <td>Nama</td>
-                    <td>No. HP</td>
-                    <td></td>
-                </thead>
-                <tbody>
-                    @foreach ($couriers as $courier)
-                        <tr>
-                            <td>{{ $courier->name }}</td>
-                            <td>{{ $courier->phone_number }}</td>
-                            <td>
-                                {{ Form::open(['url' => route('couriers.destroy', ['courier' => $courier->id]), 'method' => 'delete']) }}
-                                    {{ csrf_field() }}
-                                    <div class="btn-group" role="group" aria-label="Basic example">
-                                        <a href="{{ route('couriers.edit', ['courier' => $courier->id]) }}" class="btn btn-light">Edit</a>
-                                        {{ Form::submit('Hapus', ['class' => 'btn btn-danger']) }}
-                                    </div>
-                                {{ Form::close() }}
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            <div>
-                {{ $couriers->links() }}
-            </div>
-        @else
-            <p>Belum ada kurir yang dibuat</p>
-        @endif
+<div id="customers-list">
+    <div class="c-title row no-gutters">
+        <div class="col-sm-6">
+          <h1 class="mb-0">Courier data</h1>
+        </div>
+        <div class="col-sm-6 text-right">
+            <a class="btn btn-primary" href="{{ route('couriers.create') }}">Add courier data</a>
+        </div>
     </div>
+
+    @include('courier._table_list')
 </div>
-<span id="pageConstant" class="hidden" data-url="{{ route('couriers.index') }}"></span>
 @endsection

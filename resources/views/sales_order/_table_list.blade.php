f55de09c160ca6e9545df7a3328dfd6573b4012d
Akbar putra <akbarputrapai@gmail.com>
2019-08-24 21:04:43 +0700

    fix blankslate

--- resources/views/sales_order/_table_list.blade.php
+++ resources/views/sales_order/_table_list.blade.php
@@ -16,9 +16,9 @@
     </table>
 </div>
 @else
-<div class="text-center py-5">
-    <img src="{{ asset('assets/images/icons/shopping-bag.svg') }}" width="120" style="opacity: 0.7">
-    <h1 class="mt-3 mb-2">No data sales order</h1>
+<div class="text-center py-5 mt-5">
+    <img src="{{ asset('assets/images/icons/shopping-bag.svg') }}" width="90" style="opacity: 0.7">
+    <h2 class="mt-3 mb-2">No data sales order</h2>
     <p style="opacity: 0.5">Your sales order data will show here</p>
 </div>
 @endif

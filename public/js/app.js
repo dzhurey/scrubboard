4327cb7b962be1c520c523001f92f224ae78346b
zuhri <muhammadszuhri@gmail.com>
2019-08-27 00:58:23 +0700

    aa

--- public/js/app.js
+++ public/js/app.js
@@ -56958,8 +56958,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/appai/Documents/Personal/99 Bebewash/scrubboard/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/appai/Documents/Personal/99 Bebewash/scrubboard/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/zuhri/project/scrubboard/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/zuhri/project/scrubboard/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })

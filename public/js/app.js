769218c728ab78a24e0b42ca670d78bf18c52748
zuhri <muhammadszuhri@gmail.com>
2019-09-07 17:14:28 +0700

    cannot update order that has invoice

--- public/js/app.js
+++ public/js/app.js
@@ -58147,8 +58147,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! G:\xampp\htdocs\scrubboard\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! G:\xampp\htdocs\scrubboard\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/zuhri/project/scrubboard/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/zuhri/project/scrubboard/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/lang/en/rules.php
+++ resources/lang/en/rules.php
@@ -2,7 +2,8 @@
 
 return [
     "phone_number_not_number" => "Phone number must be a valid phone number",
-    "order_has_invoice" => "This order has invoice, delete invoice first to delete this order",
+    "cannot_delete_order_has_invoice" => "This order has invoice, delete invoice first to delete this order",
+    "cannot_update_order_has_invoice" => "This order has invoice, this order cannot be updated",
     'person_not_courier' => 'user role is not a courier',
     'data_not_found' => 'data not found',
     'item_has_transaction' => 'Item has transaction, delete transaction first to delete this item',

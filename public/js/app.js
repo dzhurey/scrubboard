c1ccea2b08bf824debcb814404f7fc33689f93d5
zuhri <muhammad.zuhri@greenhouse.co>
2019-09-25 01:48:56 +0700

    asd

--- public/js/app.js
+++ public/js/app.js
@@ -53129,7 +53129,7 @@ var generateDataPickupEdit = function generateDataPickupEdit(list_id) {
 };
 
 if (modalSalesInvoices.length > 0) {
-  _shared_index_js__WEBPACK_IMPORTED_MODULE_0__["default"].get('/api/sales_invoices?filter[]=transaction_status,=,open&filter[]=delivery_status,=,open_partial-scheduled').then(function (res) {
+  _shared_index_js__WEBPACK_IMPORTED_MODULE_0__["default"].get('/api/sales_invoices?filter[]=delivery_status,=,open_partial-scheduled').then(function (res) {
     var sales_invoices = res.sales_invoices.data;
     createSiFormTable(modalSIFormTable, sales_invoices);
   })["catch"](function (res) {

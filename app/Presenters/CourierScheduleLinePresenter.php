41d1061da32c7d7abad14104c445b893c7555697
zuhri <muhammad.zuhri@greenhouse.co>
2019-10-13 17:26:18 +0700

    update schedule estimation time to nullable

--- app/Presenters/CourierScheduleLinePresenter.php
+++ app/Presenters/CourierScheduleLinePresenter.php
@@ -26,8 +26,8 @@ class CourierScheduleLinePresenter extends BasePresenter
 
     public function transform($input)
     {
-        $estimation_time = date_create_from_format('H:i:s', $input->estimation_time);
-        $input->estimation_time = $estimation_time->format('H:i');
+        $estimation_time = !empty($input->estimation_time) ? date_create_from_format('H:i:s', $input->estimation_time) : null;
+        $input->estimation_time = !empty($estimation_time) ? $estimation_time->format('H:i') : null;
         $input->transaction_line = $this->transaction_line_presenter->transform($input->transactionLine);
         $input->transaction = $this->transaction_presenter->transform($input->transactionLine->transaction);
         $input->transaction_id = $input->transactionLine->transaction_id;

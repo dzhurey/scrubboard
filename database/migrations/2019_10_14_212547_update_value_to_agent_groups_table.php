04c021ee74746445d7761dc7ceaf9961d8806d42
zuhri <muhammad.zuhri@greenhouse.co>
2019-10-14 21:41:35 +0700

    migration to update agent group code

---
+++ database/migrations/2019_10_14_212547_update_value_to_agent_groups_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateValueToAgentGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::update('update agent_groups set agent_group_code = ? where name = ?', ['POS', 'Agent']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::update('update agent_groups set agent_group_code = ? where name = ?', ['A', 'Agent']);
+    }
+}

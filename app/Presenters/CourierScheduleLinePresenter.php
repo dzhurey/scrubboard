b97e5ad14a7a8b169b03b7317a3f640090d8cf97
zuhri <muhammad.zuhri@greenhouse.co>
2019-10-21 09:33:43 +0700

    fixing estimation time

--- app/Presenters/CourierScheduleLinePresenter.php
+++ app/Presenters/CourierScheduleLinePresenter.php
@@ -26,8 +26,14 @@ class CourierScheduleLinePresenter extends BasePresenter
 
     public function transform($input)
     {
-        $estimation_time = date_create_from_format('H:i:s', $input->estimation_time);
-        $input->estimation_time = $estimation_time->format('H:i');
+        $estimation_time = null;
+        if (!empty($input->estimation_time)) {
+            $estimation_time = date_create_from_format('H:i', $input->estimation_time);
+        }
+        if ($estimation_time == false) {
+            $estimation_time = date_create_from_format('H:i:s', $input->estimation_time);
+        }
+        $input->estimation_time = !empty($estimation_time) ? $estimation_time->format('H:i') : null;
         $input->transaction_line = $this->transaction_line_presenter->transform($input->transactionLine);
         $input->transaction = $this->transaction_presenter->transform($input->transactionLine->transaction);
         $input->transaction_id = $input->transactionLine->transaction_id;
